568c48476cc0f7055743ea72d4b9405b0d9bdf79
Deepaksharma-11 <sharmadeepak8800@gmail.com>
2025-05-19 15:42:17 +0530

    Testing

--- ArrayList/src/com/deepak/arrayInput.java
+++ ArrayList/src/com/deepak/arrayInput.java
@@ -1,11 +1,13 @@
+package com.deepak;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class arrayInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] arr = new int[10];
-        for (int i = 0; i < 10; i++) {
+        int [] arr = new int[4];
+        for (int i = 0; i < 4; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println(Arrays.toString(arr));
